cad9ac6e4950b033652069012693b0435f4c88f8
Doh Kim <ehgud456456@naver.com>
2024-08-08 11:45:15 +0900

    1768: summarize sol

--- leetcode/1768_merge_strings_alternately.py
+++ leetcode/1768_merge_strings_alternately.py
@@ -16,3 +16,27 @@ class Solution2:
         arr.append(word1[len(word2):])
         arr.append(word2[len(word1):])
         return "".join(arr)
+
+class Solution3:
+    def mergeAlternately(self, word1, word2):
+        m = len(word1)
+        n = len(word2)
+        point = max(m, n)
+        i = 0
+        result = []
+
+        while i < point:
+            if i < m:
+                result += word1[i]
+            if i < n:
+                result += word2[i]
+            i += 1
+
+        return "".join(result)
+      
+# 두 문자열의 시작부터 끝까지 순회
+# 기준을 2가지로 잡을 수 있음
+# 1.두 길이 중 긴 길이
+#   max 까지 병렬 순회. 각 문자열의 길이를 넘지 않았다면 answer에 추가
+# 2. 두 길이 중 짧은 길이
+#  중간까지 병렬 순회. 나머지 덩어리 한번에 붙이기
