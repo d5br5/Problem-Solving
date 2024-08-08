060af7c2b7b3a3449a040c34e71d13c62f787941
Doh Kim <ehgud456456@naver.com>
2024-08-08 12:26:56 +0900

    1071: fourth sol and summarize

--- leetcode/1071_greatest_common_divisor_of_strings.py
+++ leetcode/1071_greatest_common_divisor_of_strings.py
@@ -57,10 +57,28 @@ class Solution3:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
         if str1+str2 != str2+str1 :
             return ''
-        l1 = len(str1)
-        l2 = len(str2)
+        from math import gcd
+        return str1[:gcd(len(str1), len(str2))] 
 
-        gcdl = gcd(l1, l2)
-        chunk = str1[:gcdl]
+class Solution4:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        if str1+str2 != str2+str1 :
+            return ''
+        if len(str1) == len(str2):
+            return str1
+        if len(str1) > len(str2):
+            return self.gcdOfStrings(str1[len(str2):], str2)
+        return self.gcdOfStrings(str1, str2[len(str1):])
+# solution 1
+# 길이 1부터 짧은 길이까지 brute force
+# valid? 길이가 나누어져야하고, 덩어리 반복했을때 원래 문자열이 나와야 함
 
-        return chunk if chunk*(l1//gcdl) == str1 and chunk*(l2//gcdl) == str2 else ''
+# solution 3
+# 반복되는 문자열이라면, str1+str2 하든 str2+str1 하든 같은 문자열이 나옴
+# 반복 덩어리가 있다면, 그 문자열의 길이는 반드시 최대공약수와 같음
+# 1. 최대공약수보다 작은 길이 덩어리가 있다면, 최대공약수 또한 그 수의 배수기 때문에 최대 길이가 아니게 됨
+# 2. 최대공약수보다 긴 길이가 있다면, 그 수는 공약수가 아님. 
+# 따라서 반복 문자열이라면, 무조건 덩어리는 최대공약수덩어리
+
+# solution 4 : recursive 최고!!!!
+# gcd를 찾지 않고, 짧은 문자열과 다른 문자열의 그 뒷부분을 재귀적으로 비교
