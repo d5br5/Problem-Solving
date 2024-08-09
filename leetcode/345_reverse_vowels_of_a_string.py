0b79f4ccdecdd5c178f7994c47e3da662a9b8ee2
Doh Kim <ehgud456456@naver.com>
2024-08-09 18:08:54 +0900

    345: second sol

--- leetcode/345_reverse_vowels_of_a_string.py
+++ leetcode/345_reverse_vowels_of_a_string.py
@@ -13,3 +13,20 @@ class Solution:
             if answer[i] is None:
                 answer[i] = arr.pop()
         return "".join(answer)
+    
+    
+class Solution:
+    def reverseVowels(self, s: str) -> str:
+        vowels = set("AEIOUaeiou")
+        arr = list(s)
+        left = 0
+        right = len(s)-1
+
+        while left < right:
+            while left < right and arr[left] not in vowels: left += 1
+            while left < right and arr[right] not in vowels: right -= 1
+            arr[left], arr[right] = arr[right], arr[left]
+            left += 1
+            right -= 1
+        
+        return "".join(arr)
