04e6515a92ff27fe31477407403536ce18a87ce9
Doh Kim <ehgud456456@naver.com>
2024-08-19 12:47:01 +0900

    283: optimize

--- leetcode/283_move_zeros.py
+++ leetcode/283_move_zeros.py
@@ -10,15 +10,11 @@ class Solution:
             for j in range(i+1, length):
                 if nums[i] == 0 and nums[j] != 0:
                     nums[i], nums[j] = nums[j], nums[i]
-
+                    
 class Solution2:
     def moveZeroes(self, nums: List[int]) -> None:
-        zeroIndex = 0
-        
+        l = 0
         for i in range(len(nums)):
             if nums[i] != 0 :
-                nums[zeroIndex], nums[i] = nums[i], nums[zeroIndex]
-                zeroIndex += 1
-        
-        for i in range (zeroIndex, len(nums)):
-            nums[i] = 0
+                nums[l], nums[i] = nums[i], nums[l]
+                l += 1
