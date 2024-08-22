daa91b72928d2570e6d8a35c79c6de8884e5ffe3
Doh Kim <ehgud456456@naver.com>
2024-08-22 17:06:54 +0900

    392: first sol

--- leetcode/392_is_subsequence.py
+++ leetcode/392_is_subsequence.py
@@ -6,13 +6,22 @@ class Solution:
 
         def isSub(si, ti):
             if si >= slen: return True
+            if ti >= tlen: return False
 
-            sChar = s[si]
-            for i in range(ti, tlen):
-                if tlen-i < slen-si : return False
-                if t[i] == sChar:
-                    if isSub(si+1, i+1): return True
-
-            return False    
+            if s[si] == t[ti]: si+=1
+            ti+=1
+            return isSub(si, ti)
             
         return isSub(0, 0)
+      
+      
+class Solution2:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        slen, tlen = len(s), len(t)
+        si=ti=0
+
+        while si < slen and ti < tlen:
+            if s[si] == t[ti]: si+=1
+            ti+=1
+    
+        return si == slen
